2e38a5deed223b08a4c00d8b92f4898b82d2310a
nagarajuthandu <nagaraju.thandu@fortunapix.com>
2023-12-27 22:40:53 +0530

    authentication updated

--- src/app/Admin/login/login.component.ts
+++ src/app/Admin/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { SessionService } from 'src/app/services/session.service';
@@ -9,13 +9,17 @@ import { UserService } from 'src/app/services/users.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   username: string = '';
   password: string = '';
   credentials:any={}
 
   constructor(private router: Router, private sessionService : SessionService,private authService: AuthService) {}
 
+  ngOnInit():void{
+    this.authService.logout()
+  }
+  
   login():void {
     this.credentials.username =this.username
     this.credentials.password =this.password
